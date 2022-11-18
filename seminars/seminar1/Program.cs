59e564f1c22b392b2314b304d9acde366d0cc82f
filip <kfilipenko@yandex.ru>
2022-11-18 22:26:41 +0300

    save seminar

---
+++ seminars/seminar1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Console.WriteLine("Enter as number:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"Quad of {num} is {quad}");
+
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// a = 25; b = 5 -> да
+// a = 2 b = 10 -> нет
+// a = 9; b = -3 -> да
+// a = -3 b = 9 -> нет
+
+// Console.WriteLine("Enter 1-st number:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter 2-d number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad = num2 * num2;
+
+// if (quad == num1)
+// {
+//     Console.WriteLine($"{num1} is quad of {num2}");
+// }else
+// {
+//     Console.WriteLine($"{num1} is not quad of {num2}");
+// }
+
+
+// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+    
+//     456 -> 6
+    
+//     782 -> 2
+    
+//     918 -> 8
+
+// Console.WriteLine("Enter a number:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int minusNum = -num;
+
+// while(minusNum <= num){
+//     Console.Write(minusNum);
+//     minusNum++;
+// }
+
+// 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+
+/* Console.WriteLine("Enter a number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 10;
+Console.Write(result); */
