1da7695d993fa1ee021d13be1cb92ff3a2898382
filip <kfilipenko@yandex.ru>
2022-11-19 20:09:00 +0300

    2-d homework done

--- homeworks/homework1/Program.cs
+++ homeworks/homework1/Program.cs
@@ -4,23 +4,41 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
-Console.WriteLine("Enter a 1st number");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter f 2-d number");
-int b = Convert.ToInt32(Console.ReadLine());
-if (a > b){
-    Console.WriteLine($"a = {a}; b = {b} -> max {a}");
-}else if(a < b){
-    Console.WriteLine($"a = {a}; b = {b} -> max {b}");
-}else{
-    Console.WriteLine($"nubers are similar");
-}
+
+// Console.WriteLine("Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.");
+// Console.WriteLine("Enter a 1st number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter f 2-d number");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b){
+//     Console.WriteLine($"a = {a}; b = {b} -> max {a}");
+// }else if(a < b){
+//     Console.WriteLine($"a = {a}; b = {b} -> max {b}");
+// }else{
+//     Console.WriteLine($"nubers are similar");
+// }
 
 /* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
+
+Console.WriteLine("Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine("Enter a 1-st number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+Console.WriteLine("Enter a 2-nd number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 > max){
+    max = num2;
+}
+Console.WriteLine("Enter a 3-d number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num3 > max){
+    max = num3;
+}
+Console.WriteLine($"Entered numbers: {num1} {num2} {num3}, max -> {max}");
 
 /* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
