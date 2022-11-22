722335a264fe4351b9bb93c15b7bd1c2b7de0258
filip <kfilipenko@yandex.ru>
2022-11-22 23:09:26 +0300

    2-nd task done

--- homeworks/homework3/Program.cs
+++ homeworks/homework3/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// Задача 19
+﻿// Задача 19
 
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
@@ -11,22 +8,23 @@ Console.WriteLine("Hello, World!");
 
 // 23432 -> да
 
-Console.WriteLine("Enter a 5 digits number:");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter a 5 digits number:");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-string Palindrom (int number){
-    int funcNum = number;
-    int[] arr = new int[5];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = funcNum % 10;
-        funcNum = funcNum / 10;
-    }
-    if (arr[0] == arr[4] && arr[1] == arr[3]) return $"{number} -> Yes";
-    else return $"{number} -> No";
-}
+// string Palindrom (int number){
+//     int funcNum = number;
+//     int[] arr = new int[5];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = funcNum % 10;
+//         funcNum = funcNum / 10;
+//     }
+//     if (arr[0] == arr[4] && arr[1] == arr[3]) return $"{number} -> Yes";
+//     else return $"{number} -> No";
+// }
 
-Console.WriteLine(Palindrom(number));
+// Console.WriteLine(Palindrom(number));
+
 // Задача 21
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
@@ -34,6 +32,26 @@ Console.WriteLine(Palindrom(number));
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+double GetLens(int x1,int y1,int z1, int x2,int y2,int z2)
+{
+    double len = Math.Sqrt((x1-x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+    return len;
+}
+Console.WriteLine("Enter first x ordinate:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter first y ordinate:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter first z ordinate:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second x ordinate:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second y ordinate:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second z ordinate:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(GetLens(x1, y1, z1, x2, y2, z2));
 
 // Задача 23
 
@@ -43,9 +61,4 @@ Console.WriteLine(Palindrom(number));
 // 5 -> 1, 8, 27, 64, 125
 
 
-// double GetLens(int x1,int y1,int z1, int x2,int y2,int z2)
-// {
-//     double len = Math.Sqrt((x1-x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
-//     return len;
-// }
-// Console.WriteLine(GetLens(3, 6, 8, 2, 1, -7));
+
