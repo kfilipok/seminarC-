4b49298d728d26b0b4245b6e2fdb7a74ea14f21e
filip <kfilipenko@yandex.ru>
2022-11-22 23:20:30 +0300

    3 task done

--- homeworks/homework3/Program.cs
+++ homeworks/homework3/Program.cs
@@ -32,26 +32,28 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
-double GetLens(int x1,int y1,int z1, int x2,int y2,int z2)
-{
-    double len = Math.Sqrt((x1-x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
-    return len;
-}
-Console.WriteLine("Enter first x ordinate:");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter first y ordinate:");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter first z ordinate:");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter second x ordinate:");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter second y ordinate:");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter second z ordinate:");
-int z2 = Convert.ToInt32(Console.ReadLine());
 
 
-Console.WriteLine(GetLens(x1, y1, z1, x2, y2, z2));
+// double GetLens(int x1,int y1,int z1, int x2,int y2,int z2)
+// {
+//     double len = Math.Sqrt((x1-x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+//     return len;
+// }
+// Console.WriteLine("Enter first x ordinate:");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter first y ordinate:");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter first z ordinate:");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second x ordinate:");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second y ordinate:");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter second z ordinate:");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.WriteLine(GetLens(x1, y1, z1, x2, y2, z2));
 
 // Задача 23
 
@@ -59,6 +61,18 @@ Console.WriteLine(GetLens(x1, y1, z1, x2, y2, z2));
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+void Cube(int num){
+    if (num < 1) Console.WriteLine("Number must be bigger then 1");
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i}^3 = {i * i * i}");
+    }
+}
+
+Console.WriteLine("Enter a number");
+int n = Convert.ToInt32(Console.ReadLine());
+Cube(n);
 
 
 
